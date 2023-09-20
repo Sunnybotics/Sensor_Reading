b8844da4b6b0e3a5f0d14f469eff8d7978630cca
Cristian Ivan Aldana Guzmán <60593924+CIAG61@users.noreply.github.com>
2023-09-20 17:02:15 -0500

    enviando datos por metodo POST

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,68 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <WebServer.h>
-#include "ws2812.h" 
-#include "system.h" //contiene las variables del sistema
+#include <HTTPClient.h>
+#include "ws2812.h"
+#include "system.h" // Contiene las variables del sistema
 #include "multitask.h"
 
-// Definiciones de red WiFi
 const char* ssid = "ROBOTICSNEXTGY";
 const char* password = "Sunnytop2022";
 
-// Pines de los sensores agrupados
-const int group1Pins[4] = {19, 18, 12, 14};  // Grupo 1, adelante
-const int group2Pins[4] = {26, 32, 33, 35};  // Grupo 2, atras
+const int group1Pins[4] = {19, 18, 12, 14};
+const int group2Pins[4] = {26, 32, 33, 35};
 
-// Pines del relé y el buzzer
-const int relay1 = RL1; 
-const int relay2 = RL2; 
-const int buzzer = BUZZER; 
+const int relay1 = RL1;
+const int relay2 = RL2;
+const int buzzer = BUZZER;
 
-WebServer server(80);
+int group1Count = 0;
+int group2Count = 0;
+
+unsigned long lastSensorUpdateTime = 0;
+const unsigned long sensorUpdateInterval = 2; // Cambiado a 10ms
+
+void actualizarSensores() {
+  group1Count = 0;
+  group2Count = 0;
+
+  for (int i = 0; i < 4; i++) {
+    group1Count += digitalRead(group1Pins[i]) == HIGH;
+    group2Count += digitalRead(group2Pins[i]) == HIGH;
+  }
+}
+
+void controlarRelesYBuzzer() {
+  bool relay1Active = (group1Count >= 4);
+  bool relay2Active = (group2Count >= 4);
+
+  digitalWrite(relay1, relay1Active ? HIGH : LOW);
+  digitalWrite(relay2, relay2Active ? LOW : HIGH);
+
+  if (relay1Active || relay2Active) {
+    digitalWrite(buzzer, LOW);
+  } else {
+    digitalWrite(buzzer, HIGH);
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
 
-  // Configurar pines de relé y buzzer
   pinMode(relay1, OUTPUT);
-  digitalWrite(relay1, HIGH); // Inicialmente apagado
-  
+  digitalWrite(relay1, LOW);
+
   pinMode(relay2, OUTPUT);
-  digitalWrite(relay2, HIGH); // Inicialmente apagado
+  digitalWrite(relay2, HIGH);
 
   pinMode(buzzer, OUTPUT);
-  digitalWrite(buzzer, HIGH); // Inicialmente apagado
+  digitalWrite(buzzer, HIGH);
 
-  // Configurar pines de los sensores
   for (int i = 0; i < 4; i++) {
     pinMode(group1Pins[i], INPUT);
     pinMode(group2Pins[i], INPUT);
   }
 
-  // Configuración de la red WiFi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -54,187 +77,136 @@ void setup() {
   Serial.print("Dirección IP: ");
   Serial.println(WiFi.localIP());
 
-  // Configuración de las rutas para el servidor web
-  server.on("/", HTTP_GET, [](){
-    String html = "<html><body>";
-    html += "<h2>";
-    html += "<span style='color:rgb(252, 191, 0); font-size: 50px;'> Estado de </span>";
-    html +=  "<span style='color:rgb(236, 105, 7); font-size: 50px;'> Sensores </span>";
-    html += "</h2>";
 
-    int group1Count = 0;
-    int group2Count = 0;
-
-    // Leer el estado de los sensores
-    for (int i = 0; i < 4; i++) {
-      group1Count += digitalRead(group1Pins[i]) == HIGH;
-      group2Count += digitalRead(group2Pins[i]) == HIGH;
-    }
-
-    // Controlar el relé según el estado de los sensores
-    bool relay1Active = (group1Count >= 4);
-    bool relay2Active = (group2Count >= 4);
-   
-    digitalWrite(relay1, relay1Active ? LOW : HIGH); // Grupo 1
-    digitalWrite(relay2, relay2Active ? LOW : HIGH); // Grupo 2
-
-    // Mostrar estado de los sensores en la página web
-    html += "<p>Grupo 1 Sensores Activos ADELANTE: ";
-    html += group1Count;
-    html += "</p>";
-
-    html += "<p>Grupo 2 Sensores Activos ATRAS: ";
-    html += group2Count;
-    html += "</p>";
-
-    // Mostrar estado del relé y el buzzer en la página web
-  html += "<h2>FRONTAL: ";
-  html += relay1Active ? "<span style='color:red;'>ROBOT DETENIDO</span>" : "<span style='color:green;'>DENTRO DEL AREA DE TRABAJO</span>";
-  html += "</h2>";
-
-  html += "<h2>TRASERA: ";
-  html += relay2Active ? "<span style='color:red;'>ROBOT DETENIDO</span>" : "<span style='color:green;'>DENTRO DEL AREA DE TRABAJO</span>";
-  html += "</h2>";
-
-    html += "</body></html>";
-    server.send(200, "text/html", html);
-
-    // Mostrar estado de los sensores en el monitor serial
-    Serial.print("Grupo 1 Sensores Activos ADELANTE: ");
-    Serial.println(group1Count);
-    Serial.print("Grupo 2 Sensores Activos ATRAS: ");
-    Serial.println(group2Count);
-
-    // Mostrar mensaje si el relé 1 está activo
-    if (relay1Active) {
-      Serial.println("ROBOT DETENIDO: ADELANTE FUERA DEL AREA DE TRABAJO");
-    }
-    // Mostrar mensaje si el relé 2 está activo
-    if (relay2Active) {
-      Serial.println("ROBOT DETENIDO: ATRAS FUERA DEL AREA DE TRABAJO");
-    }
-    
-  });
-
-  server.begin();
 }
 
 void loop() {
-  server.handleClient();
-  // Verificar si el rele 1 o el rele 2 están activos
-  if (digitalRead(relay1) == LOW || digitalRead(relay2) == LOW) {
-    // Si cualquiera de los relés está activo, activar el buzzer
-    digitalWrite(buzzer, LOW);
-  } else {
-    // Si ninguno de los reles está activo, mantener el buzzer apagado
-    digitalWrite(buzzer, HIGH);
+
+    actualizarSensores();
+    controlarRelesYBuzzer();
+
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
+    String datos_a_enviar = "senFrontales=" + (String)group1Count + "&senTraseros=" + (String)group2Count;
+
+    http.begin("http://192.168.0.176/Servidor/insertar/guardarDatos.php");        //Indicamos el destino
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Preparamos el header text/plain si solo vamos a enviar texto plano sin un paradigma llave:valor.
+
+    int codigo_respuesta = http.POST(datos_a_enviar);
+  
+
+
+
+    if(codigo_respuesta>0){
+      Serial.println("Código HTTP ► " + String(codigo_respuesta));   //Print return code
+
+      if(codigo_respuesta == 200){
+        String cuerpo_respuesta = http.getString();
+        Serial.println("El servidor respondió ▼ ");
+        Serial.println(cuerpo_respuesta);
+
+      }
+
+    }else{
+
+     Serial.print("Error enviando POST, código: ");
+     Serial.println(codigo_respuesta);
+
+    }
+
+    http.end();  //libero recursos
+
+  }else{
+
+     Serial.println("Error en la conexión WIFI");
+
   }
+  delay(100);
 }
+
+
 
 
 
 // #include <Arduino.h>
 // #include <WiFi.h>
 // #include <WebServer.h>
-// #include "ws2812.h" 
+// #include "ws2812.h"
 // #include "system.h" //contiene las variables del sistema
 // #include "multitask.h"
-
 // // Definiciones de red WiFi
 // const char* ssid = "ROBOTICSNEXTGY";
 // const char* password = "Sunnytop2022";
-
 // // Pines de los sensores agrupados
 // const int group1Pins[4] = {19, 18, 12, 14};  // Grupo 1, adelante
 // const int group2Pins[4] = {26, 32, 33, 35};  // Grupo 2, atras
-
 // // Pines del relé y el buzzer
-// const int relay1 = RL1; 
-// const int buzzer = BUZZER; 
-
+// const int relay1 = RL1;
+// const int buzzer = BUZZER;
 // WebServer server(80);
-
 // void setup() {
 //   Serial.begin(115200);
-
 //   // Configurar pines de relé y buzzer
 //   pinMode(relay1, OUTPUT);
 //   digitalWrite(relay1, HIGH); // Inicialmente apagado
 //   pinMode(buzzer, OUTPUT);
 //   digitalWrite(buzzer, HIGH); // Inicialmente apagado
-
 //   // Configurar pines de los sensores
 //   for (int i = 0; i < 4; i++) {
 //     pinMode(group1Pins[i], INPUT);
 //     pinMode(group2Pins[i], INPUT);
 //   }
-
 //   // Configuración de la red WiFi
 //   WiFi.mode(WIFI_STA);
 //   WiFi.begin(ssid, password);
-
 //   while (WiFi.status() != WL_CONNECTED) {
 //     delay(500);
 //     Serial.print(".");
 //   }
-
 //   Serial.println("");
 //   Serial.print("Conectado a ");
 //   Serial.println(ssid);
 //   Serial.print("Dirección IP: ");
 //   Serial.println(WiFi.localIP());
-
 //   // Configuración de las rutas para el servidor web
 //   server.on("/", HTTP_GET, [](){
 //     String html = "<html><body>";
 //     html += "<h1>Estado de Sensores</h1>";
-
 //     int group1Count = 0;
 //     int group2Count = 0;
-
 //     // Leer el estado de los sensores
 //     for (int i = 0; i < 4; i++) {
 //       group1Count += digitalRead(group1Pins[i]) == HIGH;
 //       group2Count += digitalRead(group2Pins[i]) == HIGH;
 //     }
-
 //     // Controlar el relé según el estado de los sensores
 //     bool relay1Active = (group1Count >= 4);
-
 //     // Controlar el Buzzer para enviar un alto cuando se active el grupo 2
 //     bool buzzerActive = (group2Count >= 4);
-
 //     // Mostrar estado de los sensores en la página web
 //     html += "<p>Grupo 1 Sensores Activos ADELANTE: ";
 //     html += group1Count;
 //     html += "</p>";
-
 //     html += "<p>Grupo 2 Sensores Activos ATRAS: ";
 //     html += group2Count;
 //     html += "</p>";
-
 //     // Mostrar estado del relé y el buzzer en la página web
 //     html += "<h2>FRONTAL: ";
 //     html += relay1Active ? " ROBOT DETENIDO" : " DENTRO DEL AREA DE TRABAJO";
 //     html += "</h2>";
-
 //     html += "<h2>TRASERA: ";
 //     html += buzzerActive ? " ROBOT DETENIDO" : " DENTRO DEL AREA DE TRABAJO";
 //     html += "</h2>";
-
 //     html += "</body></html>";
 //     server.send(200, "text/html", html);
-
 //     // Mostrar estado de los sensores en el monitor serial
 //     Serial.print("Grupo 1 Sensores Activos ADELANTE: ");
 //     Serial.println(group1Count);
 //     Serial.print("Grupo 2 Sensores Activos ATRAS: ");
 //     Serial.println(group2Count);
 //   });
-
 //   server.begin();
 // }
-
 // void loop() {
 //   server.handleClient();
 // }
